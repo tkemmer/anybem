4dedd187425a8399c9d8403ba154ea287f645ab4
Thomas Kemmer <thomas@binaryden.de>
2018-08-13 15:45:54 +0200

    Use std::array instead of static C arrays

--- src/cpp/types.h
+++ src/cpp/types.h
@@ -1,12 +1,13 @@
 #ifndef ANYBEM_TYPES_H
 #define ANYBEM_TYPES_H
 
+#include <array>
 #include <cstdlib>
 
 namespace anybem {
 	using real_t     = double;
 	using index_t    = int64_t;
-	using position_t = real_t[3];
+	using position_t = std::array<real_t, 3>;
 }
 
 #endif // ANYBEM_TYPES_H
