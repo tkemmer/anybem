3167067d57d8bd0c00502b691f4623568ffac7a7
Thomas Kemmer <thomas@binaryden.de>
2018-08-15 13:03:15 +0200

    Add resource class for surface models

--- src/cpp/model.h
+++ src/cpp/model.h
@@ -3,6 +3,8 @@
 
 #include "types.h"
 
+#include <vector>
+
 namespace anybem {
 
 	using Node = position_t;
@@ -36,6 +38,39 @@ namespace anybem {
 		SurfaceElement* elements;
 		Charge*         charges;
 		SystemParams    params;
+	};
+
+	class SurfaceModelResource final {
+	public:
+		SurfaceModelResource(
+			std::vector<Node>&& nodes,
+			std::vector<SurfaceElement>&& elements,
+			std::vector<Charge>&& charges
+		) :
+			nodes_{nodes},
+			elements_{elements},
+			charges_{charges} {
+		}
+
+		~SurfaceModelResource() = default;
+
+		SurfaceModel get() {
+			return SurfaceModel{
+				nodes_.data(),
+				elements_.data(),
+				charges_.data(),
+				{0, 0, 0, 0} // TODO
+			};
+		}
+
+		std::vector<Node>::size_type node_count() { return nodes_.size(); }
+		std::vector<SurfaceElement>::size_type element_count() { return elements_.size(); }
+		std::vector<Charge>::size_type charge_count() { return charges_.size(); }
+
+	private:
+		std::vector<Node>           nodes_;
+		std::vector<SurfaceElement> elements_;
+		std::vector<Charge>         charges_;
 	};
 }
 
