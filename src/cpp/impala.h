a11eaf7546efe4dfcfe64fcd43305971014cbea7
Thomas Kemmer <thomas@binaryden.de>
2018-08-15 10:09:43 +0200

    Add anybem_charge_buffer_stats function

--- src/cpp/impala.h
+++ src/cpp/impala.h
@@ -3,4 +3,5 @@
 extern "C" {
 	void anybem_node_buffer_stats(anybem::position_t* nodes, anybem::index_t len);
 	void anybem_surface_element_buffer_stats(anybem::Triangle* elements, anybem::index_t len);
+	void anybem_charge_buffer_stats(anybem::Charge* charges, anybem::index_t len);
 }
