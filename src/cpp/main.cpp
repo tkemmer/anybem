9f720189d76ab86d839663a51aead0b4b3047bf7
Thomas Kemmer <thomas@binaryden.de>
2018-08-16 15:06:33 +0200

    Allow to change system parameters via cli

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -12,10 +12,20 @@ using namespace std::string_literals;
 
 int main(int argc, char** argv) {
 	auto hmo_file = ""s;
+	auto params   = default_params;
 
-	group cli{
-		value("hmo file"s, hmo_file) % "Input file with a triangulated surface model and charges"s
-	};
+	auto cli = (
+		"Input file with a triangulated surface model and charges"s %
+			value("hmo file"s, hmo_file),
+		("Dielectric constant of the solute (default: "s + to_string(params.epsOmega) + ")"s) %
+			option("--eps-omega"s) & value("val"s, params.epsOmega),
+		("Dielectric constant of the solvent (default: "s + to_string(params.epsSigma) + ")"s) %
+			option("--eps-sigma"s) & value("val"s, params.epsSigma),
+		("Large-scale solvent response (default: "s + to_string(params.epsInf) + ")"s) %
+			option("--eps-inf"s) & value("val"s, params.epsInf),
+		("Correlation length scale (in Angstrom; default: "s + to_string(params.lambda) + ")"s) %
+			option("--lambda"s) & value("val"s, params.lambda)
+	);
 
 	if(!parse(argc, argv, cli)) {
 		cout << make_man_page(cli, argv[0])
@@ -24,14 +34,15 @@ int main(int argc, char** argv) {
 	}
 
 	HMOReader reader;
-	auto sh = reader.readSurfaceModel(hmo_file);
+	auto surf = reader.readSurfaceModel(hmo_file);
+	surf.params(params);
 
 	cout << "HMO file statistics:\n"s
-	     << " * Nodes:    "s << to_string(sh.node_count()) << "\n"s
-	     << " * Elements: "s << to_string(sh.element_count()) << "\n"s
-	     << " * Charges:  "s << to_string(sh.charge_count()) << "\n"s
+	     << " * Nodes:    "s << to_string(surf.node_count()) << "\n"s
+	     << " * Elements: "s << to_string(surf.element_count()) << "\n"s
+	     << " * Charges:  "s << to_string(surf.charge_count()) << "\n"s
 	     << "\n"s;
 
-	auto surf = sh.prototype();
-	anybem_surface_model_stats(&surf);
+	auto sproto = surf.prototype();
+	anybem_surface_model_stats(&sproto);
 }
