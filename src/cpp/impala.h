d1d335f5d63542edbbe8420663098b8dd7558f0d
Thomas Kemmer <thomas@binaryden.de>
2018-08-16 11:33:23 +0200

    Fix anybem_* interface functions

--- src/cpp/impala.h
+++ src/cpp/impala.h
@@ -1,8 +1,8 @@
 #include "types.h"
 
 extern "C" {
-	void anybem_node_buffer_stats(anybem::Node* nodes, anybem::index_t len);
-	void anybem_surface_element_buffer_stats(anybem::Triangle* elements, anybem::index_t len);
-	void anybem_charge_buffer_stats(anybem::Charge* charges, anybem::index_t len);
+	void anybem_node_buffer_stats(anybem::NodeBuffer::Prototype* nodes);
+	void anybem_surface_element_buffer_stats(anybem::TriangleBuffer::Prototype* elements);
+	void anybem_charge_buffer_stats(anybem::ChargeBuffer::Prototype* charges);
 	void anybem_surface_model_stats(anybem::SurfaceModel::Prototype* surf);
 }
