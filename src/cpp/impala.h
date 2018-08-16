a1e08136cf4b4135638d48ef9568f3cbbb3b91e2
Thomas Kemmer <thomas@binaryden.de>
2018-08-16 15:37:36 +0200

    Make interface function parameters const

--- src/cpp/impala.h
+++ src/cpp/impala.h
@@ -1,8 +1,8 @@
 #include "types.h"
 
 extern "C" {
-	void anybem_node_buffer_stats(anybem::NodeBuffer::Prototype* nodes);
-	void anybem_surface_element_buffer_stats(anybem::TriangleBuffer::Prototype* elements);
-	void anybem_charge_buffer_stats(anybem::ChargeBuffer::Prototype* charges);
-	void anybem_surface_model_stats(anybem::SurfaceModel::Prototype* surf);
+	void anybem_node_buffer_stats(const anybem::NodeBuffer::Prototype* nodes);
+	void anybem_surface_element_buffer_stats(const anybem::TriangleBuffer::Prototype* elements);
+	void anybem_charge_buffer_stats(const anybem::ChargeBuffer::Prototype* charges);
+	void anybem_surface_model_stats(const anybem::SurfaceModel::Prototype* surf);
 }
