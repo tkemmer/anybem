854e594fbc2268111d72b1e7333495c0dc7644bd
Thomas Kemmer <thomas@binaryden.de>
2018-08-17 16:58:36 +0200

    Remove unused anybem_* interface functions

--- src/cpp/impala.h
+++ src/cpp/impala.h
@@ -4,9 +4,6 @@
 #include "model.h"
 
 extern "C" {
-	void anybem_node_buffer_stats(const anybem::NodeBuffer::Prototype* nodes);
-	void anybem_surface_element_buffer_stats(const anybem::TriangleBuffer::Prototype* elements);
-	void anybem_charge_buffer_stats(const anybem::ChargeBuffer::Prototype* charges);
 	void anybem_surface_model_stats(const anybem::SurfaceModel::Prototype* surf);
 }
 
